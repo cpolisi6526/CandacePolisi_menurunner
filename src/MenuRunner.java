ecf8e50e251a81defc88aeb1f6d34ebb871469f7
BT_1N3_26 <cpolisi6526@bths.edu>
2018-11-27 14:59:02 -0500

    22

--- src/MenuRunner.java
+++ src/MenuRunner.java
@@ -7,6 +7,12 @@ public class MenuRunner {
         Drink icedTea= new Drink("Iced Tea", 1.50);
         Drink smoothie= new Drink("smoothie",5.50);
 
-        Trio
+        Trio trio1= new Trio(reuben, waldorf, icedTea);
+        Trio trio2= new Trio(bec,caesar,smoothie);
+        Trio trio3= new Trio(bec, caesar, icedTea);
+
+        System.out.println(trio1.getName()+" Price: " + trio1.getPrice());
+        System.out.println(trio2.getName()+" Price: " + trio2.getPrice());
+        System.out.println(trio3.getName()+" Price: " + trio3.getPrice());
     }
 }
